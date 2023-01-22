c1cbcf7cf9affdad6437aeea4fb0c0845534773c
Tuzbay <t.uzbayyelce@gmail.com>
2023-01-22 23:18:59 +0300

    Slider added

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -2,7 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 function Announcement() {
-  return <div>Announcement</div>;
+  return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
 }
 
 export default Announcement;
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
