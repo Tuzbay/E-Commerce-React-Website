0e5a105e1e9ca8fd55a90f79e136f36a15e6bd02
Tuzbay <t.uzbayyelce@gmail.com>
2023-01-23 15:45:32 +0300

    Register and Login added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Product from "./pages/Product";
 // import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
+// import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="app">
       {/* <Home /> */}
-      <ProductList />
+      {/* <ProductList /> */}
+      {/* <Product /> */}
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Product.js
@@ -0,0 +1,168 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+
+import Announcement from "../components/Announcement";
+
+import Newsletter from "../components/Newsletter";
+
+import Footer from "../components/Footer";
+import { Add, Remove } from "@mui/icons-material";
+
+function Product() {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas hic
+            iste eveniet, officia deserunt error, vero labore neque dolorum
+            placeat quod eius ullam perspiciatis repudiandae, odit nesciunt
+            minima voluptas debitis. Voluptates, in modi?{" "}
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+}
+
+export default Product;
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  display: flex;
+  padding: 50px;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1; ;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+
+const Price = styled.div`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 40%;
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
