2474a92486fe932e1d4d48e8a968ba2395bcf801
Tuzbay <t.uzbayyelce@gmail.com>
2023-01-23 13:35:28 +0300

    Shopping Category added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      {/* <Home /> */}
+      <ProductList />
     </div>
   );
 }
